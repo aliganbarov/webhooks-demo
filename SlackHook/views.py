c68b123cd02a9d0ee2a435833f98f6dd9abeb6b6
aganbarov1994@gmail.com <aganbarov1994@gmail.com>
2018-11-16 18:01:11 +0100

    Bug fix

--- SlackHook/views.py
+++ SlackHook/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 class SlackHookView(TemplateView):
